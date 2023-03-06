19d19858fc53a281c2011e3f002d71ea1af18e77
EMukhlygin <95917693+GrafKenar@users.noreply.github.com>
2023-03-06 18:44:03 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_languages(browser):
+def test_basket_button_presence(browser):
     browser.get(link)
     basket_button = browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form > button')
     assert basket_button is not None
