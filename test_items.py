c227ce5abfdd7408e34fe4319092ac545b9505c8
EMukhlygin <95917693+GrafKenar@users.noreply.github.com>
2023-03-06 18:51:47 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,4 +6,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_basket_button_presence(browser):
     browser.get(link)
     basket_button = browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form > button')
-    assert basket_button is not None
+    assert basket_button is not None, "there is no such button"
