bb2ce3d3bf25c3853abb42dd441786e574ec8672
EMukhlygin <95917693+GrafKenar@users.noreply.github.com>
2023-03-06 18:30:35 +0300

    Delete test_different_languages directory
    deleting unnecessary folder with wrong files

--- test_different_languages/test_items.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-def test_languages(browser, language_expected_result):
-    browser.get(link)
-    basket_button_text = browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form > button').text
-    assert basket_button_text == language_expected_result, f"actual result is {text} while expected result is {language_expected_result}"
